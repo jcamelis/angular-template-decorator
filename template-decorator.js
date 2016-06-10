83a682d28fafb3108bc17aa142e8de3ba9d80a7f
Javier Camelis <jcamelis@gmail.com>
2016-06-10 16:59:12 -0300

    add some jsdocs

--- template-decorator.js
+++ template-decorator.js
@@ -3,7 +3,14 @@ angular.module('templateDecorator', [])
     .config(['$provide', 'templateDecoratorProvider', function($provide, templateDecoratorProvider) {
 
         $provide.decorator('$templateRequest', ['$delegate', function $templateCacheDecorator($originalTemplateRequest) {
-
+            /**
+             * @name $templateRequestDecorator
+             *
+             * @description Decorator for $templateRequest
+             *
+             * @param {string|TrustedResourceUrl} tpl The HTTP request template URL
+             * @param {boolean=} ignoreRequestError Whether or not to ignore the exception when the request fails or the template is empty
+             **/
             function $templateRequestDecorator(templateUrl, ignoreRequestError) {
 
                 if (templateDecoratorProvider.get(templateUrl)) {
@@ -28,22 +35,35 @@ angular.module('templateDecorator', [])
     }])
 
     .provider('templateDecorator', function () {
+        /**
+         * @type {Object.<String, String>}
+         */
         var decorators = {};
-
+        /**
+         * @param  {String} templateUrl
+         * @param  {String} decoratorTemplateUrl
+         * @returns {void}
+         */
         function put(templateUrl, decoratorTemplateUrl) {
-            if (templateUrl !== decoratorTemplateUrl) {
+            if (templateUrl !== decoratorTemplateUrl && angular.isString(templateUrl) && angular.isString(decoratorTemplateUrl)) {
                 decorators[templateUrl] = decoratorTemplateUrl;
             }
         };
 
         this.put = put;
 
+        /**
+         * @param  {String} templateUrl
+         */
         function get(templateUrl) {
             return decorators[templateUrl];
         };
 
         this.get = get;
 
+        /**
+         * @returns {Object}
+         */
         this.$get = function () {
             return {
                 get: get,
